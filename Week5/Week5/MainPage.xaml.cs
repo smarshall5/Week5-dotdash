ba29ca2a8b40e261d1e50f87edba449c8eb4f1b1
smarshall5 <smarshall5@my.wctc.edu>
2020-02-18 19:31:07 -0600

    Add project files.

---
+++ Week5/Week5/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week5
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string entryText = "";
+        string outText = "";
+         void ClickOnDot(object sender, EventArgs args)
+        {
+            entryText += ".";
+
+        }
+        void ClickOnDash(object sender, EventArgs args) {
+            entryText += "-";
+        }
+        void ClickOnSubmit(object sender, EventArgs args)
+        {
+
+            char text = Morse.MorseCoder(entryText);
+            outText += text;
+            entryText = "";
+            output.Text = outText;
+
+        }
+
+    }
+}
